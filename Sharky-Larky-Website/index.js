26a2c1d238615071d666c279c81bc5008551a427
Sharky <162178252+SharkyLarky@users.noreply.github.com>
2024-03-04 22:14:15 -0500

    Added temp files
    UHmm

---
+++ Sharky-Larky-Website/index.js
@@ -0,0 +1,14 @@
+const JSCLicker_info = [
+    
+]
+
+//Function - Project Selected Content change
+function projectSelected(proj) {
+    document.getElementById('poster').src= 'Images/' + proj + 'Poster.jpg'
+    document.getElementById('project-info').innerHTML = 'd'
+}
+
+//Functin - myFunc
+function myfunction() {
+    //Inesrt Function here
+}
